0e2f6e908b71f12171cc44f7ccc334a9ad8aa52b
acf-patrick <miharisoap@gmail.com>
2022-03-16 11:36:59 +0300

    list <-> data base

--- assets/php/register.php
+++ assets/php/register.php
@@ -7,7 +7,11 @@ $db = getDataBase();
 $loan = $db->query("SELECT responsible_id, amount, created_at, repayment_end_date FROM loan")->fetchAll(PDO::FETCH_OBJ);
 $responsible = $db->query("SELECT first_name, last_name FROM responsible")->fetchAll(PDO::FETCH_OBJ);
 
-foreach ($loan as &$obj)
-    $obj["responsible"] = $responsible[$obj->responsible_id];
+foreach ($loan as &$obj) {
+    $field = $responsible[$obj->responsible_id];
+    $obj->responsible = $field->first_name . ' ' . $field->last_name;
+}
+
+echo json_encode($loan);
 
 ?>
